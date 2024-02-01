c44a530f0d5cba562cdc68b6a27722ea9e423244
Daniel <daniel.marcin.a@gmail.com>
2024-02-01 22:26:15 +0100

    Changing browser router to Hash router

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
-import { BrowserRouter as Router } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
 import { Header } from './components/UI/Header/Header';
 import { BlogProvider } from './components/context/BlogContext';
 import { Main } from './components/UI/Main/Main';
 import { Footer } from './components/UI/Footer/Footer';
 import {UserProvider} from './components/context/UsersContext'
-// ... inne importy
+
 
 export default function App() {
   return (
