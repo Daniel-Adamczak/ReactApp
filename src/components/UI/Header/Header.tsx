3726e11a2184e5a3e7a1fb241409a20fff1a8712
Daniel <daniel.marcin.a@gmail.com>
2024-02-01 22:06:00 +0100

    fixing paths

--- src/components/UI/Header/Header.tsx
+++ src/components/UI/Header/Header.tsx
@@ -7,19 +7,21 @@ export const Header = () => {
   return (
     <header className='flex flex-row justify-between text-slate-400 p-3 shadow-[0_0_20px_gray] mb-4 align-text-bottom'>
       <div className='flex flex-row gap-3'>
-        <img src='../vite.svg' alt='' className='h-7' />
-        <img src='../React.png' alt='' className='h-7' />
-        <img src='../typescript.svg' alt='' className='h-7' />
-        <img src='../tailwind.png' alt='' className='h-7' />
-        <img src='../ReactRouter.png' alt='' className='h-7' />
+        <img src='./../vite.svg' alt='' className='h-7' />
+        <img src='./../React.png' alt='' className='h-7' />
+        <img src='./../typescript.svg' alt='' className='h-7' />
+        <img src='./../tailwind.png' alt='' className='h-7' />
+        <img src='./../ReactRouter.png' alt='' className='h-7' />
       </div>
 
       <h1 className='h-full align-bottom text-2xl'>My company</h1>
       <div className='flex flex-row gap-3'>
-       
-        {authenticatedUser && <Link to='/settings'><ManageAccount /></Link>}
+        {authenticatedUser && (
+          <Link to='/settings'>
+            <ManageAccount />
+          </Link>
+        )}
         <Navbar />
-        
       </div>
     </header>
   );
