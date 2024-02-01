17650989b2a82a81f0bf133575bb66d98ef2b9a7
Daniel <daniel.marcin.a@gmail.com>
2024-02-01 22:37:44 +0100

    adding source maps

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,12 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    sourcemap: true, // enable production source maps
+  },
+  css: {
+    devSourcemap: true // enable CSS source maps during development
+  },
   base: '/ReactApp/',
   plugins: [react()],
 })
