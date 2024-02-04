2c884bd37ea2647314bbf695ebf8ed213bd7d604
Daniel <daniel.marcin.a@gmail.com>
2024-02-04 18:53:08 +0100

    Changing paths of images in header

--- src/components/UI/Header/Header.tsx
+++ src/components/UI/Header/Header.tsx
@@ -7,11 +7,23 @@ export const Header = () => {
   return (
     <header className='flex flex-row justify-between text-slate-400 p-3 shadow-[0_0_20px_gray] mb-4 align-text-bottom'>
       <div className='flex flex-row gap-3'>
-        <img src='/vite.svg' alt='Vite logo' className='h-7' />
-        <img src='/React.png' alt='React logo' className='h-7' />
-        <img src='/typescript.svg' alt='TypeScript logo' className='h-7' />
-        <img src='/tailwind.png' alt='Tailwind CSS logo' className='h-7' />
-        <img src='/ReactRouter.png' alt='React Router logo' className='h-7' />
+        <img src='/assets/vite.svg' alt='Vite logo' className='h-7' />
+        <img src='/assets/React.png' alt='React logo' className='h-7' />
+        <img
+          src='/assets/typescript.svg'
+          alt='TypeScript logo'
+          className='h-7'
+        />
+        <img
+          src='/assets/tailwind.png'
+          alt='Tailwind CSS logo'
+          className='h-7'
+        />
+        <img
+          src='/assets/ReactRouter.png'
+          alt='React Router logo'
+          className='h-7'
+        />
       </div>
 
       <h1 className='h-full align-bottom text-2xl'>My company</h1>
