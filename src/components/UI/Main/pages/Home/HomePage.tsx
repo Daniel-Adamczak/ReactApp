b2d498630698a08cc1561fe3776ad2d8cf067ff9
Daniel <daniel.marcin.a@gmail.com>
2024-02-05 23:25:02 +0100

    Changing Homepage Layout

--- src/components/UI/Main/pages/Home/HomePage.tsx
+++ src/components/UI/Main/pages/Home/HomePage.tsx
@@ -1,18 +1,19 @@
-
 import { useUser } from '../../../../context/UsersContext';
 import { RegisterForm } from './RegisterForm';
 import { GetRandomUserData } from './GetRandomUserData';
 import { HomePageMainContent } from './HomePageMainContent';
 export const HomePage = () => {
-  const {authenticatedUser}= useUser();
+  const { authenticatedUser } = useUser();
   return (
     <div className=' min-h-[90vh] p-16 shadow-[0_0_10px_10px] shadow-blue-950'>
+      {!authenticatedUser && (
+        <div className='flex flex-row justify-between '>
+          <RegisterForm />
+          <GetRandomUserData />
+        </div>
+      )}
 
-     { !authenticatedUser && (<div><RegisterForm />
-      <GetRandomUserData/></div>)}
-
-      { authenticatedUser && <HomePageMainContent />}
-
+      {authenticatedUser && <HomePageMainContent />}
     </div>
   );
 };
