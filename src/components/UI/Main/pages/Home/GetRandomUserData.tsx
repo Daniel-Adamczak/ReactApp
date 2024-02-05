d2783c188cc907ee401d860f242423ab5eacf2ab
Daniel <daniel.marcin.a@gmail.com>
2024-02-05 23:27:01 +0100

    Changing Homepage Layout

--- src/components/UI/Main/pages/Home/GetRandomUserData.tsx
+++ src/components/UI/Main/pages/Home/GetRandomUserData.tsx
@@ -10,7 +10,7 @@ export const GetRandomUserData = () => {
     };
 
     return (
-        <div className='  rounded-xl bg-slate-300  opacity-70 text-lg p-6 flex flex-col  font-semibold gap-3'>
+        <div className=' max-h-fit  rounded-xl bg-slate-300  opacity-70 text-lg p-6 flex flex-col  font-semibold gap-3'>
             <h2>Register with random users data</h2>
             <button className='border rounded-md p-1 border-violet-600 hover:text-[#ffc600] hover:bg-black' onClick={handleGetRandomUser}>Get Random User</button>
         </div>
